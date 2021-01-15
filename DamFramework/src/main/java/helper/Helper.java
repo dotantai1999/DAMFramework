93e6310ca1a6202ab0b585d8dbfb9c3048caad27
dotantai1999 <dotantai.1999@gmail.com>
2021-01-15 22:44:43 +0700

    add helper functions

--- DamFramework/src/main/java/helper/Helper.java
+++ DamFramework/src/main/java/helper/Helper.java
@@ -2,11 +2,14 @@ package helper;
 
 import annotation.Column;
 import annotation.OneToOne;
+import annotation.Table;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Helper {
     public static Object mapEntityDataWithResultSet(Class entityClass, ResultSet rs){
@@ -50,4 +53,62 @@ public class Helper {
 
         return false;
     }
+
+    public static Class getClassObjectWithClassName(String className) {
+        Class clazz = null;
+        try {
+            clazz = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return clazz;
+    }
+
+    public static String getTableName(Class zClass) {
+
+        if (zClass.isAnnotationPresent(Table.class)) {
+            Table table = (Table) zClass.getAnnotation(Table.class);
+            return table.name();
+        }
+        return "";
+    }
+
+    public static List<String> getListColumn(Class zClass) {
+        List<String> result = new ArrayList<>();
+        if (zClass.isAnnotationPresent(Table.class)) {
+            Field[] fields = zClass.getDeclaredFields();
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(Column.class)) {
+                    Column column = field.getAnnotation(Column.class);
+                    result.add(column.name());
+                }
+            }
+        }
+        return result;
+    }
+
+    public static List<String> getListAttributeName(Class zClass) {
+        List<String> result = new ArrayList<>();
+        if (zClass.isAnnotationPresent(Table.class)) {
+            Field[] fields = zClass.getDeclaredFields();
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(Column.class)) {
+                    String column = field.getName();
+                    result.add(column);
+                }
+            }
+        }
+        return result;
+    }
+
+    // select * from Fresher fresher , Address address join Customer customer where ...
+    public static String getClassNameFromQuery(String query) {
+        String result = "";
+        if (query.toLowerCase().startsWith("select")) {
+            result = query.split("from ")[1].split(" ")[0];
+        }
+        return result;
+    }
+
 }
